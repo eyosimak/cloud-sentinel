58a184e24014fcd515bb951823bc5061983890a8
eyosimak <eyoseyas37@gmail.com>
2026-07-29 13:16:13 +0300

    making a little sentinel for your dev work

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to cloud sentinel!"}
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
